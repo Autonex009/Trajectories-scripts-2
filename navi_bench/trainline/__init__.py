ab848564dad9ce9f39804cada89f687ff2cca731
shuklaShubhendu <drs79sjiw@gmail.com>
2026-04-13 21:57:16 +0530

    v3 changes

---
+++ navi_bench/trainline/__init__.py
@@ -0,0 +1,11 @@
+"""Trainline verifier module for train search navigation."""
+
+from navi_bench.trainline.trainline_url_match import (
+    TrainlineUrlMatch,
+    generate_task_config,
+)
+
+__all__ = [
+    "TrainlineUrlMatch",
+    "generate_task_config",
+]
